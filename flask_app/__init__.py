767715a332ca7de300bf406f3d64a42db40ef47b
Jeremiah Douglas <jeremiahdouglas323@yahoo.com>
2024-07-09 19:47:37 -0400

    fixed things up

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)  # Initialize Flask-Bcrypt with the Flask app instance
+
+# Set a secret key for session management
+app.secret_key = 'Keep_It_Low_Key'  # Set to your specified secret key
+
+# Initialize Flask-Bcrypt with the Flask app instance
+bcrypt = Bcrypt(app)
 
 # Custom filter to truncate text
 @app.template_filter('truncate')
 def truncate(s, length):
     return s if len(s) <= length else s[:length] + '...'
 
-# Import and register blueprints or routes
+# Import routes
 from flask_app.controllers import users
 from flask_app.controllers import games
-
